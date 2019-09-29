b38b55a39663c4947085d2d9e2ef9f23ec8ba71c
Alexey Voronkov <pitityy56@gmail.com>
2019-09-29 12:20:24 -0700

    creating project structure

---
+++ barry-service/src/main/java/com/werter/barryservice/BarryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.werter.barryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BarryServiceApplication.class, args);
+    }
+
+}
---
+++ larry-service/src/main/java/com/werter/larryservice/LarryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.werter.larryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LarryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LarryServiceApplication.class, args);
+    }
+
+}
